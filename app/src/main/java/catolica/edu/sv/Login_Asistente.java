2253a73130ab19cc63c606cb1d5561915f5f267e
Genesis Brito <genesis.brito@catolico.edu.sv>
2023-11-10 01:00:43 -0600

    new

---
+++ app/src/main/java/catolica/edu/sv/Login_Asistente.java
@@ -0,0 +1,14 @@
+package catolica.edu.sv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_Asistente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_asistente);
+    }
+}
---
+++ app/src/main/java/catolica/edu/sv/Login_Dentista.java
@@ -0,0 +1,14 @@
+package catolica.edu.sv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_Dentista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_dentista);
+    }
+}
